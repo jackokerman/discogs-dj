9e77a46d0ed034582613314d3594d3966a5fcdc2
Jack Okerman <jackokerman@gmail.com>
2016-06-09 21:55:59 -0700

    Add .babelrc to fix webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -34,9 +34,9 @@ module.exports = {
       test: /\.js?$/,
       exclude: /node_modules/,
       loader: 'babel',
-      query: {
-        "presets": ["react", "es2015", "stage-0", "react-hmre"]
-      }
+      // query: {
+      //   "presets": ["react", "es2015", "stage-0", "react-hmre"]
+      // }
     }, {
       test: /\.json?$/,
       loader: 'json'
